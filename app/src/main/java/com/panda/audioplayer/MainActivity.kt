6a82ac4de531ff746fcd8f326aa61799274b1d02
Panda-Young <1641140221@qq.com>
2025-04-05 14:27:38 +0800

    Initialize interface components

--- app/src/main/java/com/panda/audioplayer/MainActivity.kt
+++ app/src/main/java/com/panda/audioplayer/MainActivity.kt
@@ -1,20 +1,79 @@
 package com.panda.audioplayer
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var playPauseButton: Button
+    private lateinit var prevButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var seekBar: SeekBar
+    private lateinit var audioTitle: TextView
+    private lateinit var audioArtist: TextView
+    private lateinit var audioCover: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize UI components
+        playPauseButton = findViewById(R.id.play_pause_button)
+        prevButton = findViewById(R.id.prev_button)
+        nextButton = findViewById(R.id.next_button)
+        seekBar = findViewById(R.id.seek_bar)
+        audioTitle = findViewById(R.id.audio_title)
+        audioArtist = findViewById(R.id.audio_artist)
+        audioCover = findViewById(R.id.audio_cover)
+
+        // Set up button listeners
+        playPauseButton.setOnClickListener {
+            togglePlayPause()
         }
+
+        prevButton.setOnClickListener {
+            playPrevious()
+        }
+
+        nextButton.setOnClickListener {
+            playNext()
+        }
+
+        // Set up seek bar listener
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Do nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Do nothing
+            }
+        })
+    }
+
+    private fun togglePlayPause() {
+        // TODO: Implement play/pause logic
+    }
+
+    private fun playPrevious() {
+        // TODO: Implement previous track logic
+    }
+
+    private fun playNext() {
+        // TODO: Implement next track logic
+    }
+
+    private fun seekTo(progress: Int) {
+        // TODO: Implement seek logic
     }
 }
